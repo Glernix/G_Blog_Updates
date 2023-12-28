ae55c09d426c0e396e8d04ee6aee105799379287
Roshan Mohapatra <roshanmohapatra2004@gmail.com>
2023-12-28 18:24:34 +0530

    Add files via upload

---
+++ G_scripts.js
@@ -0,0 +1,6 @@
+// JavaScript function to open a webpage in a new tab
+function openWebpageInNewTab(url) {
+    // Use window.open with '_blank' to open the URL in a new tab
+    window.open(url, '_blank');
+  }
+  
